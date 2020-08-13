9f3aac5aa7cc8b5228a1eda5e5993be0f1714a25
Ahmed Baligh <ahmedm11@hotmail.com>
2020-08-13 05:54:10 +0200

    Add questions and category retrieval tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -41,25 +41,61 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
-    def test_question_deletion(self):
-        res = self.client().delete('/questions/10')
+    # Get Categories
+    def test_categories_retrieval(self):
+        res = self.client().get('/categories')
         data = json.loads(res.data)
 
-        question = Question.query.filter(Question.id == 10).one_or_none()
-
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(question, None)
-        self.assertEqual(data['deleted'], 10)
         self.assertTrue(data['success'])
-        self.assertTrue(data['total_questions'])
-        self.assertTrue(len(data['questions']))
+        self.assertTrue(len(data['categories']))
+        self.assertTrue(data['total_categories'])
 
-    def test_question_creation(self):
-        res = self.client().post('/questions', json=self.new_question)
+    def test_questions_retrieval(self):
+        res = self.client().get('/questions')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+
+    def test_404_no_questions_page(self):
+        res = self.client().get('/questions?page=100')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+
+    # Delete a question
+    # def test_question_deletion(self):
+    #     res = self.client().delete('/questions/10')
+    #     data = json.loads(res.data)
+
+    #     question = Question.query.filter(Question.id == 10).one_or_none()
+
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(question, None)
+    #     self.assertEqual(data['deleted'], 10)
+    #     self.assertTrue(data['success'])
+    #     self.assertTrue(data['total_questions'])
+    #     self.assertTrue(len(data['questions']))
+
+    # Post a question
+    # def test_question_creation(self):
+    #     res = self.client().post('/questions', json=self.new_question)
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+
+    # Search for a question
+
+    # Get questions by category
+
+    # Quiz
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
