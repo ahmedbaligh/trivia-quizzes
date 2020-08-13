5016e714abe1c5d542ae23aed31b215fd369d162
Ahmed Baligh <ahmedm11@hotmail.com>
2020-08-13 18:16:33 +0200

    Edit to category fetch

--- frontend/src/components/QuizView.js
+++ frontend/src/components/QuizView.js
@@ -56,7 +56,7 @@ class QuizView extends Component {
       contentType: 'application/json',
       data: JSON.stringify({
         previous_questions: previousQuestions,
-        quiz_category: this.state.quizCategory,
+        category: this.state.quizCategory.id,
       }),
       xhrFields: {
         withCredentials: true,
